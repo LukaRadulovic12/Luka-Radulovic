f44148e69c20389a88954254210f8611cc78e76d
relja456 <reljakucevic@gmail.com>
2026-07-02 20:29:19 +0200

    dsadsa

--- VESALA IGRA HANGMAN/index.cjs
+++ VESALA IGRA HANGMAN/index.cjs
@@ -2,24 +2,28 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 const { reciZaVesala } = require("./recizavesala");
 
 app.use(cors());
 
+const BROJ_POKUSAJA = 14;
+
 let izabranaRec = "";
+let trenutnoStanjePokusaja;
 
 app.get("/api/nova-igra", async (request, response) => {
-  let trenutnoStanjePokusaja = 10;
+  trenutnoStanjePokusaja = BROJ_POKUSAJA;
   const izabraniIndeks = Math.floor(Math.random() * reciZaVesala.length);
   izabranaRec = reciZaVesala[izabraniIndeks];
+
+  console.log("izabranaRec");
+  console.log(izabranaRec);
 
   response.json({
     duzina: izabranaRec.length,
     trenutnoStanjePokusajares: trenutnoStanjePokusaja,
   });
-
-  console.log(izabranaRec);
 });
 
 app.get("/api/pogodi-slovo", (request, response) => {
@@ -33,9 +37,16 @@ app.get("/api/pogodi-slovo", (request, response) => {
     }
   }
 
-  response.json(nizPogodjenihSlova);
+  if (nizPogodjenihSlova.length === 0) {
+    console.log("Smanjio sam broj");
+    trenutnoStanjePokusaja -= 1;
+  }
+
+  console.log("[PogodiSlovo] Resposne:");
+  console.log({ nizPogodjenihSlova, trenutnoStanjePokusaja });
+  response.json({ nizPogodjenihSlova, trenutnoStanjePokusaja });
 });
 
 app.listen(PORT, () => {
-  console.log("SERVER IS RUNNING ON PORT 3000");
+  console.log("SERVER IS RUNNING ON PORT " + PORT);
 });
--- VESALA IGRA HANGMAN/script.js
+++ VESALA IGRA HANGMAN/script.js
@@ -1,28 +1,35 @@
 console.log("TEST TEST TEST");
 
-const backendURL = "http://127.0.0.1:3000";
+const backendURL = "http://127.0.0.1:3002";
 const prikazReci = document.getElementById("prikazReci");
 const buttonNovaIgra = document.getElementById("buttonNovaIgra");
 const prikazPokusaja = document.getElementById("prikazPokusaja");
 const inputSlova = document.getElementById("inputSlova");
 const buttonSlova = document.getElementById("buttonSlova");
+const brojPokusaja = document.getElementById("prikazPokusaja");
 
 async function pronadjiNovuRec() {
   const responseIzabranaRec = await fetch(`${backendURL}/api/nova-igra`);
   let dataIzabranaRec = await responseIzabranaRec.json();
+
   const duzinaIzabraneReci = dataIzabranaRec.duzina;
   const trenutnoStanjePokusaja = dataIzabranaRec.trenutnoStanjePokusajares;
+
   const h1trenutnoStanjePokusaja = document.createElement("h1");
+
   h1trenutnoStanjePokusaja.innerText = trenutnoStanjePokusaja;
   console.log(trenutnoStanjePokusaja);
+
   for (let i = 0; i < duzinaIzabraneReci; i++) {
     const h1Slovo = document.createElement("h1");
     h1Slovo.innerText = "_";
     h1Slovo.id = i;
     prikazReci.appendChild(h1Slovo);
   }
+
   prikazPokusaja.appendChild(h1trenutnoStanjePokusaja);
 }
+
 buttonNovaIgra.addEventListener("click", () => {
   pronadjiNovuRec();
 });
@@ -30,14 +37,24 @@ buttonNovaIgra.addEventListener("click", () => {
 async function pronadjiNovoSlovo() {
   const inputSlovaSlovo = String(inputSlova.value);
 
-  const responseUnetoSlovo = await fetch(
-    `${backendURL}/api/pogodi-slovo?slovo=${inputSlovaSlovo}`,
-  );
+  const responseUnetoSlovo = await fetch(`${backendURL}/api/pogodi-slovo?slovo=${inputSlovaSlovo}`);
 
-  const dataUnetoSlovo = await responseUnetoSlovo.json();
+  const { nizPogodjenihSlova, trenutnoStanjePokusaja } = await responseUnetoSlovo.json();
+  osveziTrenutnoStanjePokusaja(trenutnoStanjePokusaja);
 
-  console.log(dataUnetoSlovo);
+  console.log(nizPogodjenihSlova);
+  console.log("trenutnoStanjePokusaja: " + trenutnoStanjePokusaja);
+
+  for (const index of nizPogodjenihSlova) {
+    const h1El = document.getElementById(String(index));
+    h1El.innerText = String(inputSlovaSlovo);
+  }
 }
+
 buttonSlova.addEventListener("click", async (request, response) => {
   pronadjiNovoSlovo();
 });
+
+function osveziTrenutnoStanjePokusaja(trenutniBrojPokusaja) {
+  prikazPokusaja.innerText = trenutniBrojPokusaja;
+}
